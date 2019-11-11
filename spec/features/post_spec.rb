1806ffe9005ec168cd980a6390a4da8ba7ab3959
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-11 04:23:22 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,15 +6,9 @@ describe 'new post' do
     expect(page.status_code).to eq(200)
   end
   
-  require 'rails_helper'
- 
-describe 'new post' do
- 
-  ...
  
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
   end
-end
 end
